fce1a991d870c13ee304c74e82f60c53cef81624
unknown <Tcubais@yandex.ru>
2021-03-20 11:58:16 +0900

    first

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from "./components/kit/Header/index"
+
+function App() {
+  return (
+    <div className="App">
+    <Header>
+       </Header>
+    </div>
+  );
+}
+
+export default App;
